22f29b62243deccce4937fc32a0f7c65f653c18d
Øyvind Kolås <pippin@gimp.org>
2016-08-05 23:56:58 +0200

    do more events in event example - no longer fit for single slide ....

--- docs/34-events.c
+++ docs/34-events.c
@@ -1,5 +1,6 @@
 #include "mrg.h"
 #include "mrg-string.h"
+#include <string.h>
 void make_big ();
 
 MrgEvent event_copy;
@@ -19,14 +20,50 @@ void ui (Mrg *mrg, void *data) {
      cairo_scale (mrg_cr (mrg), 0.8, 0.8);
      cairo_translate (mrg_cr(mrg), -mrg_width(mrg)/2,-mrg_height(mrg)/2);
    }
+
+ mrg_text_listen (
+      mrg, MRG_DRAG | MRG_PRESS | MRG_RELEASE | MRG_CROSSING | MRG_SCROLL | MRG_TAP | MRG_TAP_AND_HOLD,
+      handle_event_cb,
+      NULL, NULL);
+ mrg_printf (mrg, "[many]");
+ mrg_text_listen_done (mrg);
+
  mrg_text_listen (
       mrg, MRG_DRAG,
       handle_event_cb,
       NULL, NULL);
-/* there is also a _full version, which takes
-destroy_notify, and a destroy_notify_data args*/
- mrg_printf (mrg, "[event trap]\n");
+ mrg_printf (mrg, "[drag]");
  mrg_text_listen_done (mrg);
+ mrg_text_listen (
+      mrg, MRG_PRESS | MRG_RELEASE,
+      handle_event_cb,
+      NULL, NULL);
+ mrg_printf (mrg, "[press]");
+ mrg_text_listen_done (mrg);
+ mrg_text_listen (
+      mrg, MRG_SCROLL,
+      handle_event_cb,
+      NULL, NULL);
+ mrg_printf (mrg, "[scroll]");
+ mrg_text_listen_done (mrg);
+
+ mrg_text_listen (
+      mrg, MRG_TAP | MRG_TAP_AND_HOLD,
+      handle_event_cb,
+      NULL, NULL);
+ mrg_printf (mrg, "[tap]");
+ mrg_text_listen_done (mrg);
+
+ mrg_text_listen (
+      mrg, MRG_CROSSING,
+      handle_event_cb,
+      NULL, NULL);
+ mrg_printf (mrg, "[crossing]");
+ mrg_text_listen_done (mrg);
+ mrg_printf (mrg, "\n");
+
+ mrg_listen (mrg, MRG_KEY_DOWN, handle_event_cb, NULL, NULL);
+ mrg_listen (mrg, MRG_KEY_UP, handle_event_cb, NULL, NULL);
 
  mrg_printf (mrg, "type=");
  switch (event_copy.type)
@@ -34,10 +71,12 @@ destroy_notify, and a destroy_notify_data args*/
    case MRG_PRESS: mrg_print (mrg, "MRG_PRESS"); break;
    case MRG_MOTION: mrg_print (mrg, "MRG_MOTION"); break;
    case MRG_RELEASE: mrg_print (mrg, "MRG_RELEASE"); break;
+   case MRG_SCROLL: mrg_print (mrg, "MRG_ENTER"); break;
    case MRG_ENTER: mrg_print (mrg, "MRG_ENTER"); break;
    case MRG_LEAVE: mrg_print (mrg, "MRG_LEAVE"); break;
    case MRG_KEY_DOWN: mrg_print (mrg, "MRG_KEY_DOWN"); break;
    case MRG_KEY_UP: mrg_print (mrg, "MRG_KEY_UP"); break;
+   case MRG_TAP: mrg_print (mrg, "MRG_TAP"); break;
    case MRG_DRAG_PRESS: mrg_print (mrg, "MRG_DRAG_PRESS"); break;
    case MRG_DRAG_MOTION: mrg_print (mrg, "MRG_DRAG_MOTION"); break;
    case MRG_DRAG_RELEASE: mrg_print (mrg, "MRG_DRAG_RELEASE"); break;
@@ -51,6 +90,8 @@ destroy_notify, and a destroy_notify_data args*/
  {
    mrg_printf (mrg, "unicode=%if", event_copy.unicode);
    mrg_printf (mrg, "key_name=\"%s\"", event_copy.key_name);
+   if (!strcmp (event_copy.key_name, "control-q"))
+     mrg_quit (mrg); //exit(0);
  }
  else
  {
@@ -62,8 +103,6 @@ destroy_notify, and a destroy_notify_data args*/
    mrg_printf (mrg, "delta_x=%2.1f\n", event_copy.delta_x);
    mrg_printf (mrg, "delta_y=%2.1f\n", event_copy.delta_y);
  }
-
- 
 }
 
 int main () {
