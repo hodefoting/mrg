223705f96999473234a15cce84ac4e8537f7236c
Øyvind Kolås <pippin@gimp.org>
2018-03-25 15:20:22 +0200

    mrg-utf8: move implementations to be inline functions in header

--- lib/mrg-utf8.c
+++ lib/mrg-utf8.c
@@ -1,18 +1,6 @@
 #include <string.h>
 #include "mrg-utf8.h"
 
-int mrg_utf8_len (const unsigned char first_byte)
-{
-  if      ((first_byte & 0x80) == 0)
-    return 1; /* ASCII */
-  else if ((first_byte & 0xE0) == 0xC0)
-    return 2;
-  else if ((first_byte & 0xF0) == 0xE0)
-    return 3;
-  else if ((first_byte & 0xF8) == 0xF0)
-    return 4;
-  return 1;
-}
 
 int
 mrg_unichar_to_utf8 (unsigned int  ch,
@@ -47,36 +35,3 @@ mrg_unichar_to_utf8 (unsigned int  ch,
     return 0;
 }
 
-int mrg_utf8_strlen (const char *s)
-{
-   int count;
-   if (!s)
-     return 0;
-   for (count = 0; *s; s++)
-     if ((*s & 0xC0) != 0x80)
-       count++;
-   return count;
-}
-
-const char *mrg_utf8_skip (const char *string, int utf8_length)
-{
-  const char *s;
-  int len;
-  int pos = 0;
-
-  if (!string)
-    return NULL;
-
-  for (s = string; pos < utf8_length && *s; s += mrg_utf8_len (*s))
-    {
-      int c;
-      len = mrg_utf8_len (*s);
-      for (c = 0; c < len; c++)
-        {
-          if (!s[c])
-            return s;
-        }
-      pos++;
-    }
-  return s;
-}
--- lib/mrg-utf8.h
+++ lib/mrg-utf8.h
@@ -1,9 +1,47 @@
 #ifndef MRG_UTF8_H
 #define MRG_UTF8_H
 
-int          mrg_utf8_len              (const unsigned char first_byte);
-int          mrg_utf8_strlen           (const char *s);
-const char  *mrg_utf8_skip             (const char *string, int utf8_length);
+#include <string.h>
+
+static inline int mrg_utf8_len (const unsigned char first_byte)
+{
+  if      ((first_byte & 0x80) == 0)
+    return 1; /* ASCII */
+  else if ((first_byte & 0xE0) == 0xC0)
+    return 2;
+  else if ((first_byte & 0xF0) == 0xE0)
+    return 3;
+  else if ((first_byte & 0xF8) == 0xF0)
+    return 4;
+  return 1;
+}
+
+static inline int mrg_utf8_strlen (const char *s)
+{
+   int count;
+   if (!s)
+     return 0;
+   for (count = 0; *s; s++)
+     if ((*s & 0xC0) != 0x80)
+       count++;
+   return count;
+}
+
+static inline const char *mrg_utf8_skip (const char *s, int utf8_length)
+{
+   int count;
+   if (!s)
+     return NULL;
+   for (count = 0; *s; s++)
+   {
+     if ((*s & 0xC0) != 0x80)
+       count++;
+     if (count == utf8_length+1)
+       return s;
+   }
+   return s;
+}
+
 int          mrg_unichar_to_utf8       (unsigned int ch,
                                         unsigned char*dest);
 
